35f988cc0939ec2fab3c952d2ceaf992a3ee4f68
Yashwanth A <yash2706.git@gmail.com>
2025-12-29 12:24:47 +0000

    fix: genai import

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -4,7 +4,7 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
-import google as genai
+import google.genai as genai
 from datetime import datetime
 import uuid
 from django.views.decorators.csrf import csrf_exempt
